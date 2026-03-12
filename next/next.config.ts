29dabafedfa52a550dfd7cdb3ac46891acd0f4b1
owner <owner@localhost>
2026-03-12 11:25:46 +0900

    改ページ

--- next/next.config.ts
+++ next/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export', // これで静的HTMLとしてビルド可能
+  trailingSlash: true, // これで /about/index.html になる
 };
 
 export default nextConfig;
