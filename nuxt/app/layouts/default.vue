f20c12b9f0ed90ad43bb58bde8b1045a366a079a
owner <owner@localhost>
2026-03-13 22:04:15 +0900

    動作確認

---
+++ nuxt/app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Top</NuxtLink></li>
+          <li><NuxtLink to="/development">Dev</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <slot />
+    <footer>footer</footer>
+  </div>
+</template>
---
+++ nuxt/app/pages/development.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    development.vue
+  </div>
+</template>
